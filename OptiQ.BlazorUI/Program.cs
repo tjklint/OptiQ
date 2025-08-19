071f1cb810bd48ec0257a21b7e53ee46e7782804
TJ Klint <112092190+tjklint@users.noreply.github.com>
2025-08-18 23:56:47 -0400

    refactor: remove JSON serialization configuration for WebAssembly

--- OptiQ.BlazorUI/Program.cs
+++ OptiQ.BlazorUI/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OptiQ.BlazorUI.Components;
 using OptiQ.BlazorUI.Services;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,16 +15,6 @@ builder.Services.AddScoped(sp =>
 {
     var httpClient = new HttpClient { BaseAddress = new Uri(apiBaseUrl) };
     return httpClient;
-});
-
-// Configure JSON serialization options for WebAssembly compatibility
-builder.Services.Configure<JsonSerializerOptions>(options =>
-{
-    options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    options.PropertyNameCaseInsensitive = true;
-    options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-    options.NumberHandling = JsonNumberHandling.AllowReadingFromString;
-    options.WriteIndented = false;
 });
 
 // Add services
